0422d4b80faa3b8e6a833592ca9492331a76e2f4
zaaachos <zachariadis.gs@gmail.com>
2024-06-08 19:35:25 +0300

    fix openai and html

--- src/api/app.py
+++ src/api/app.py
@@ -52,80 +52,81 @@ if not rag_client.check_vector_fullness():
     rag_client.upload_full_data(chat_client, custom_dataloader, chunk_splitter)
 
 
-# @app.get("/", response_class=HTMLResponse)
-# async def get(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/", response_class=HTMLResponse)
+async def get(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-# @app.websocket("/chat")
-# async def chat_endpoint(websocket: WebSocket):
-#     # Accept incoming websocket connection
-#     await websocket.accept()
-#     # Add the websocket to the list of clients
-#     websocket_clients.append(websocket)
+@app.websocket("/chat")
+async def chat_endpoint(websocket: WebSocket):
+    # Accept incoming websocket connection
+    await websocket.accept()
+    # Add the websocket to the list of clients
+    websocket_clients.append(websocket)
 
-#     try:
-#         # initialise the role of the system
-#         messages = list()
-#         messages.append(
-#             {
-#                 "role": "system",
-#                 "content": """You are Q&A bot. A highly intelligent system that answers user questions based on the 'CONTEXT' provided by the user above
-#                             each 'QUESTION'. Be Specific and Descriptive. Order matters, so if for example user tells you 'summarize the following...', you have to summarize the
-#                             query provided after the word 'following'. If you don't know the answer, please think rationally answer your own knowledge base.""",
-#             }
-#         )
+    try:
+        # initialise the role of the system
+        messages = list()
+        messages.append(
+            {
+                "role": "system",
+                "content": """You are Q&A bot. A highly intelligent system that answers user questions based on the 'CONTEXT' provided by the user above
+                            each 'QUESTION'. Be Specific and Descriptive. Order matters, so if for example user tells you 'summarize the following...', you have to summarize the
+                            query provided after the word 'following'. If you don't know the answer, please think rationally answer your own knowledge base.""",
+            }
+        )
 
-#         while True:
-#             # Receive message from the client
-#             user_message = await websocket.receive_text()
-#             # use RAG to retrieve relevant text-passages
-#             retrieved = chat_client.rag_query(query_text=user_message)
+        while True:
+            # Receive message from the client
+            user_message = await websocket.receive_text()
+            # use RAG to retrieve relevant text-passages
+            user_query_text_embeds = chat_client.generate_text_embeds(query_text=user_message)
+            retrieved = rag_client.retrieve(text_embeddings=user_query_text_embeds)
 
-#             top_k_matches = retrieved["matches"]
-#             contexts = [m["metadata"]["text"] for m in top_k_matches]
+            top_k_matches = retrieved["matches"]
+            contexts = [m["metadata"]["text"] for m in top_k_matches]
 
-#             # create the context message to pass to LLM
-#             context_str = "\n\n---\n\n".join(contexts) + "\n\n-----\n\n"
-#             print(context_str)
-#             memory.append(context_str)
+            # create the context message to pass to LLM
+            context_str = "\n\n---\n\n".join(contexts) + "\n\n-----\n\n"
+            print(context_str)
+            memory.append(context_str)
 
-#             # Combine RAG contexts with past interactions from memory
-#             combined_context = "\n\n".join(contexts + list(memory))
-#             messages.append(
-#                 {"role": "user", "content": f"'CONTEXT': {combined_context}"}
-#             )
+            # Combine RAG contexts with past interactions from memory
+            combined_context = "\n\n".join(contexts + list(memory))
+            messages.append(
+                {"role": "user", "content": f"'CONTEXT': {combined_context}"}
+            )
 
-#             # now pass the user message to model
-#             messages.append({"role": "user", "content": f"'QUESTION': {user_message}"})
-#             memory.append(context_str)
-#             # Send message to Azure OpenAI and get response
-#             chat_client_response = await get_openai_response(websocket, messages)
-#             memory.append(chat_client_response)
+            # now pass the user message to model
+            messages.append({"role": "user", "content": f"'QUESTION': {user_message}"})
+            memory.append(context_str)
+            # Send message to Azure OpenAI and get response
+            chat_client_response = await get_openai_response(websocket, messages)
+            memory.append(chat_client_response)
 
-#             # then append to history messages
-#             messages.append({"role": "assistant", "content": chat_client_response})
+            # then append to history messages
+            messages.append({"role": "assistant", "content": chat_client_response})
 
-#     finally:
-#         # Remove the websocket from the list of clients if connection is closed
-#         websocket_clients.remove(websocket)
+    finally:
+        # Remove the websocket from the list of clients if connection is closed
+        websocket_clients.remove(websocket)
 
 
-# async def get_openai_response(websocket, message: str) -> str:
-#     async with AsyncClient() as client:
+async def get_openai_response(websocket, message: str) -> str:
+    async with AsyncClient() as client:
 
-#         response = chat_client.respond(message)
-#         responses = []
-#         for chunk in response:
-#             if len(chunk.choices) > 0:
-#                 msg = chunk.choices[0].delta.content
-#                 msg = "" if msg is None else msg
-#                 responses.append(msg)
+        response = chat_client.respond(message)
+        responses = []
+        for chunk in response:
+            if len(chunk.choices) > 0:
+                msg = chunk.choices[0].delta.content
+                msg = "" if msg is None else msg
+                responses.append(msg)
 
-#                 await websocket.send_text(msg)
+                await websocket.send_text(msg)
 
-#         response = "".join(response)
-#         return response
+        response = "".join(response)
+        return response
 
 
 if __name__ == "__main__":
